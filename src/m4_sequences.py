b4cb9e4c9f2625cc9bd0207d9137446f695302cc
yangz4 <yangz4@rose-hulman.edu>
2019-01-23 20:14:17 -0500

    zhen yang

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -367,8 +367,12 @@ def run_test_practice_problem4d():
 def practice_problem4d(sequence):
     sum = 0
     for k in range(len(sequence)):
-        if is_prime(sequence[k]) and sequence[k] != sequence[k+1]:
-            sum += sequence[k]
+        if k == len(sequence):
+            if is_prime(sequence[k]):
+                sum += sequence[k]
+        else:
+            if is_prime(sequence[k]) and sequence[k]!=sequence[k+1]:
+                sum += sequence[k]
     return sum
 
     """
@@ -420,7 +424,7 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
