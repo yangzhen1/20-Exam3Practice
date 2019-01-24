79e038732fcdc6e22d102e3bb7091851022fcee6
yangz4 <yangz4@rose-hulman.edu>
2019-01-23 20:43:17 -0500

    zhen yang

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zhen Yang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -56,6 +56,17 @@ def run_test_shape():
 
 
 def shape(r):
+    for j in range(r):
+        for s in range(j):
+            print(' ', end='')
+        for k in range(r-j):
+            print('+', end='')
+        print('!', end='')
+        for a in range(1, r-j+1):
+            print(a, end='')
+        for b in range(j):
+            print('-', end='')
+        print()
     """
     Prints a shape with r rows that looks like this example where r=7:
     +++++++!7654321
@@ -76,7 +87,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ###########################################################################
